e0c0d1ed975e93b3e170c8dcd3452d1c4e8c2585
jetlechleb <64711655+jetlechleb@users.noreply.github.com>
2023-05-27 00:09:20 +0200

    Fixed request body processing

--- src/main/java/com/michal/movies/controller/MoviesController.java
+++ src/main/java/com/michal/movies/controller/MoviesController.java
@@ -3,14 +3,17 @@ package com.michal.movies.controller;
 import com.michal.movies.controller.model.Movie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController("/movies")
+@RestController
+@RequestMapping("/movies")
 public class MoviesController {
 
     @PostMapping
-    public void createMovie(Movie movie) {
+    public void createMovie(@RequestBody Movie movie) {
         log.info("I just received: " + movie.toString());
     }
 }
--- src/main/java/com/michal/movies/controller/model/Movie.java
+++ src/main/java/com/michal/movies/controller/model/Movie.java
@@ -1,6 +1,8 @@
 package com.michal.movies.controller.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
