6fd02006dfabe20d21be1959ae0440fcc5063625
jetlechleb <64711655+jetlechleb@users.noreply.github.com>
2023-06-23 00:15:27 +0200

    Fixed request body processing

--- src/main/java/com/michal/movies/controller/MoviesController.java
+++ src/main/java/com/michal/movies/controller/MoviesController.java
@@ -1,20 +1,80 @@
 package com.michal.movies.controller;
 
 import com.michal.movies.controller.model.Movie;
+import com.michal.movies.repository.MovieRepository;
+import com.michal.movies.repository.model.MovieEntity;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 @Slf4j
 @RestController
 @RequestMapping("/movies")
+@RequiredArgsConstructor
 public class MoviesController {
+
+    private final MovieRepository movieRepository;
 
     @PostMapping
     public void createMovie(@RequestBody Movie movie) {
         log.info("I just received: " + movie.toString());
+
+        MovieEntity movieEntity = MovieEntity.builder()
+                .year(movie.getYear())
+                .actors(movie.getActors())
+                .directors(movie.getDirectors())
+                .genre(movie.getGenre())
+                .title(movie.getTitle())
+                .id(UUID.randomUUID())
+                .build();
+
+        movieRepository.save(movieEntity);
+    }
+
+    @GetMapping("/{id}")
+    public Movie getMovieById(@PathVariable("id") Long id) {
+        return Movie.builder()
+                .year(2000)
+                .genre("horror")
+                .title("Ciganska rapsodie")
+                .actors(List.of("michal mrosko", "michal dord", "stepan kozub"))
+                .build();
+
+        // Implementation for retrieving a movie with the given ID
+        // Return the retrieved movie object
+    }
+
+    @GetMapping
+    public List<Movie> getAllMovies() {
+
+        List<Movie> movies = new ArrayList<>();
+
+        for (MovieEntity movieEntity : movieRepository.findAll()) {
+            Movie movie = Movie.builder()
+                    .year(movieEntity.getYear())
+                    .genre(movieEntity.getGenre())
+                    .title(movieEntity.getTitle())
+                    .actors(movieEntity.getActors())
+                    .build();
+            movies.add(movie);
+        }
+        return movies;
+
+    }
+
+
+    @PutMapping("/{id}")
+    public void updateMovie(@PathVariable("id") Long id, @RequestBody Movie updatedMovie) {
+        // Implementation for updating a movie with the given ID using the updatedMovie object
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteMovie(@PathVariable("id") Long id) {
+        // Implementation for deleting a movie with the given ID
     }
 }
 
--- src/main/java/com/michal/movies/controller/model/Movie.java
+++ src/main/java/com/michal/movies/controller/model/Movie.java
@@ -1,14 +1,15 @@
 package com.michal.movies.controller.model;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
+@Builder
 public class Movie {
 
+    private Long id;
     private String title;
     private List<String> directors;
     private List<String> actors;
